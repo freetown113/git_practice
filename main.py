0c6ee493ca45566c15d5e202da582608a9dc5b2b
freetown113 <freetown113@gmail.com>
2022-11-24 14:07:21 +0200

    refactored code addording to PEP8

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 
@@ -12,11 +11,12 @@ class Combinator:
     def get_combinations(self, number=1, max_length=1, min_length=2):
         result = list([None])*number
         for i in range(number):
-            item = random.sample(self.seq, random.randint(min_length, max_length))
+            item = random.sample(self.seq, random.randint(min_length,
+                                 max_length))
             result[i] = ''.join(item)
         return result
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     com = Combinator()
     print(print(com.get_combinations(5, 6)))
