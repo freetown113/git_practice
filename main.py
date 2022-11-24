a5220d6e0437d8cb09573c8b3bcb20e05b4ecce4
freetown113 <freetown113@gmail.com>
2022-11-24 16:07:48 +0200

    refactor methods declaration according to PEP8

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import random
+from typing import List
 
 
 class Combinator:
-    def __init__(self):
+    def __init__(self) -> None:
         self.seq = self.__handle_seq('abcdefghigklmnopqrstuvwxyz')
 
-    def __handle_seq(self, seq):
+    def __handle_seq(self,
+                     seq: str
+                     ) -> List:
         return list(seq)
 
-    def get_combinations(self, number=1, max_length=1, min_length=2):
+    def get_combinations(self,
+                         number: int = 1,
+                         max_length: int = 1,
+                         min_length: int = 2
+                         ) -> str:
         result = list([None])*number
         for i in range(number):
             item = random.sample(self.seq, random.randint(min_length,
